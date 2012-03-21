965a5242ef6e564e47b38c46f1a38e34d1007b99
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-21 15:37:28 +0000

    Publish extension to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sprite/trunk@59498 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sprite".
 #
-# Auto generated 05-03-2012 10:30
+# Auto generated 21-03-2012 16:36
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,37 +11,37 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Sprite',
-	'description' => 'Creates sprites with typoscript very easily.',
-	'category' => 'Frontend',
-	'author' => 'Armin Ruediger Vieweg',
-	'author_email' => 'armin@v.ieweg.de',
-	'author_company' => '',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '0.0.1',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '4.5.0-0.0.0',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"269d";s:17:"ext_localconf.php";s:4:"105a";s:14:"ext_tables.php";s:4:"0643";s:14:"ext_tables.sql";s:4:"d41d";s:29:"Classes/class.tx_dragdrop.php";s:4:"9fe2";s:41:"Classes/XClass/class.ux_tx_cms_layout.php";s:4:"5070";s:33:"Resources/Public/CSS/dragdrop.css";s:4:"1553";s:39:"Resources/Public/JavaScript/dragdrop.js";s:4:"b050";}',
+    'title' => 'Sprite',
+    'description' => 'Creates sprites with typoscript very easily.',
+    'category' => 'Frontend',
+    'author' => 'Armin Ruediger Vieweg',
+    'author_email' => 'armin@v.ieweg.de',
+    'author_company' => '',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '4.5.0-0.0.0',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+    '_md5_values_when_last_written' => 'a:13:{s:16:"ext_autoload.php";s:4:"6734";s:12:"ext_icon.gif";s:4:"8255";s:17:"ext_localconf.php";s:4:"0643";s:14:"ext_tables.php";s:4:"70f6";s:14:"ext_tables.sql";s:4:"d41d";s:33:"Classes/class.spritegenerator.php";s:4:"0cad";s:29:"Classes/class.tx_dragdrop.php";s:4:"9fe2";s:28:"Classes/class.userSprite.php";s:4:"66d4";s:30:"Classes/Utility/TypoScript.php";s:4:"3291";s:41:"Classes/XClass/class.ux_tx_cms_layout.php";s:4:"5070";s:34:"Configuration/TypoScript/setup.txt";s:4:"81a9";s:33:"Resources/Public/Images/blank.gif";s:4:"3254";s:14:"doc/manual.sxw";s:4:"7418";}',
 );
 
 ?>
